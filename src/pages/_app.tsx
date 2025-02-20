38fc3231407e34ad0f06bb3e62230d8d006aa677
LuAn <63506861+ND-Luan@users.noreply.github.com>
2025-02-20 10:47:01 +0700

    refactor theme configuration for admin and client, remove unused theme file, and add new theme files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 
-import theme from '@/theme/themeConfig';
+import theme from '@/theme/theme_admin_config';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <ConfigProvider theme={theme}>
---
+++ src/theme/theme_admin_config.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from "antd";
+
+const themeAdminConfig: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#52c41a",
+  },
+};
+
+export default themeAdminConfig;
